60f5bd352f649e6e1feee3d8b81210823900fe7f
lordgrimzxc <joshuadejesus0017@gmail.com>
2026-01-08 10:17:54 +0800

    added folder

---
+++ lib/screens/auth/signin_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatefulWidget {
+  const SigninScreen({super.key});
+
+  @override
+  State<SigninScreen> createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        //LOGO
+
+        //Welcome Message
+
+        //Username Textfield
+
+        //Password Textfield
+
+        //Sign In Button
+        
+        //Sign Up TextButton
+      ],)
+    );
+  }
+}
