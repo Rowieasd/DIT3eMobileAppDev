a6f64cf7241566279d5f8bb0468fddd473c7ebe3
lordgrimzxc <joshuadejesus0017@gmail.com>
2026-01-15 10:16:24 +0800

    New file added

--- lib/screens/auth/signin_screen.dart
+++ lib/screens/auth/signin_screen.dart
@@ -11,19 +11,48 @@ class _SigninScreenState extends State<SigninScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        //LOGO
+      body: Column(
+        children: [
+          //LOGO
+          SizedBox(height: 50),
+          Center(
+            child: Container(
+              child: Image.asset(
+                'assets/images/logoecommerce.jpg',
+                width: 180,
+                height: 180,
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
 
-        //Welcome Message
+          //Welcome Message
+          Text("SIGN IN"),
+          SizedBox(height: 10),
+          //Username Textfield
+          TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.account_box_sharp),
+              labelText: "Username",
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.purple),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.purple),
+              ),
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          SizedBox(height: 10),
+          //Password Textfield
 
-        //Username Textfield
+          //Sign In Button
 
-        //Password Textfield
-
-        //Sign In Button
-        
-        //Sign Up TextButton
-      ],)
+          //Sign Up TextButton
+        ],
+      ),
     );
   }
 }
